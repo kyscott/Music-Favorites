5d97536b86abf280f1bb271c86b25f28037bd80e
Kyle Scott <kylescott@Kyles-MacBook-Pro-2.local>
2017-10-23 21:14:38 -0400

    refactoring code to use map to display info

--- client/src/components/ArtistPage/EventModal.js
+++ client/src/components/ArtistPage/EventModal.js
@@ -1,101 +1,70 @@
 import React from 'react';
 import './css/EventModal.css';
+
+import moment from 'moment';
+
 const EventModal = props => {
-  return (
-<div className="modal fade" id="eventsModal" role="dialog">
-    <div className="modal-dialog">
-    
-      <div className="modal-content">
-        <div className="modal-header">
-          <button type="button" className="close" data-dismiss="modal">&times;</button>
-          <h4 className="modal-title">Upcoming Events for {props.artistName}</h4>
-        </div>
-        <div className="modal-body">
 
-<table className="table table-hover">
-    <thead>
-      <tr>
-        <th>Name: </th>
-        <th>Date:</th>
-        <th>Time:</th>
-        <th>Venue:</th>
-        <th>Location:</th>
-      </tr>
-    </thead>
+const events = props.events;
+console.log(events)
 
-    <tbody>
-      <tr>
-        <td><a className="event-link" href={props.eventUrl01} target="_blank">{props.eventName01}</a></td>
-        <td>{props.eventDate01}</td>
-        <td>{props.eventTime01}</td>
-        <td><a className="event-link" href={props.venueUrl01} target="_blank">{props.venue01}</a></td>
-        <td>{props.location01}</td>
-      </tr>
+// const eventList = events.map((event, index) => {
+//     <tr>
+//         <td><a className="event-link" href={event.uri} target="_blank">{event.displayName}</a></td>
+//         <td>{moment(event.start.date).format("MMM Do YY")}</td>
+//         <td>{moment(event.start.time, 'HH:mm').format('hh:mm a')}</td>
+//         <td><a className="event-link" href={event.venue.uri} target="_blank">{event.venue.displayName}</a></td>
+//         <td>{event.location.city}</td>
+//     </tr>
+// })
 
-      <tr>
-        <td><a className="event-link" href={props.eventUrl02} target="_blank">{props.eventName02}</a></td>
-        <td>{props.eventDate02}</td>
-        <td>{props.eventTime02}</td>
-        <td><a className="event-link" href={props.venueUrl02} target="_blank">{props.venue02}</a></td>
-        <td>{props.location02}</td>
-      </tr>
+return (
+      <div className="modal fade" id="eventsModal" role="dialog">
+         <div className="modal-dialog">
+            <div className="modal-content">
 
-      <tr>
-        <td><a className="event-link" href={props.eventUrl03} target="_blank">{props.eventName03}</a></td>
-        <td>{props.eventDate03}</td>
-        <td>{props.eventTime03}</td>
-        <td><a className="event-link" href={props.venueUrl03} target="_blank">{props.venue03}</a></td>
-        <td>{props.location03}</td>
-      </tr>
+               <div className="modal-header">
+                  <button type="button" className="close" data-dismiss="modal">&times;</button>
+                  <h4 className="modal-title">Upcoming Events for {props.artistName}</h4>
+               </div>
 
-      <tr>
-        <td><a className="event-link" href={props.eventUrl04} target="_blank">{props.eventName04}</a></td>
-        <td>{props.eventDate04}</td>
-        <td>{props.eventTime04}</td>
-        <td><a className="event-link" href={props.venueUrl04} target="_blank">{props.venue04}</a></td>
-        <td>{props.location04}</td>
-      </tr>
+               <div className="modal-body">
+                  <table className="table table-hover">
+                     <thead>
+                        <tr>
+                           <th>Name: </th>
+                           <th>Date:</th>
+                           <th>Time:</th>
+                           <th>Venue:</th>
+                           <th>Location:</th>
+                        </tr>
+                     </thead>
 
-      <tr>
-        <td><a className="event-link" href={props.eventUrl05} target="_blank">{props.eventName05}</a></td>
-        <td>{props.eventDate05}</td>
-        <td>{props.eventTime05}</td>
-        <td><a className="event-link" href={props.venueUrl05} target="_blank">{props.venue05}</a></td>
-        <td>{props.location05}</td>
-      </tr>
-    </tbody>
+                     <tbody>
+                        {/*{eventList}*/}
+                        <tr>
+                           <td><a className="event-link" href={props.eventUrl} target="_blank">{props.eventName}</a></td>
+                           <td>{props.eventDate}</td>
+                           <td>{props.eventTime}</td>
+                           <td><a className="event-link" href={props.venueUrl} target="_blank">{props.venue}</a></td>
+                           <td>{props.location}</td>
+                        </tr>
+                     </tbody>
 
-  </table>
+                  </table>
+               </div>
 
-        </div>
-        <div className="modal-footer">
-          <a href="http://www.songkick.com" target="_blank" alt="songkick.com">
-            <img className="songkick-logo" src="https://concertmap.azureedge.net/graphics/powered-by-songkick-pink.png" alt="Powered by Songkick"/>
-          </a>
-          <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-        </div>
+               <div className="modal-footer">
+                  <a href="http://www.songkick.com" target="_blank" alt="songkick.com">
+                     <img className="songkick-logo" src="https://concertmap.azureedge.net/graphics/powered-by-songkick-pink.png" alt="Powered by Songkick" />
+                  </a>
+                  <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+               </div>
+            </div>
+            
+         </div>
       </div>
-      
-    </div>
-  </div>
   )
 }
 
 export default EventModal;
-
-// for (var i = 0; i < data.resultsPage.results.event.length; i++) {
-//    // EVENT NAME -- new <td>
-//    console.log(data.resultsPage.results.event[i].displayName);
-//    console.log(data.resultsPage.results.event[i].uri);
-
-//    // EVENT DATE -- new <td>
-//    console.log(data.resultsPage.results.event[i].start.date);
-//    console.log(data.resultsPage.results.event[i].start.time);
-
-//    // EVENT VENUE -- new <td>
-//    console.log(data.resultsPage.results.event[i].venue.displayName);
-//    console.log(data.resultsPage.results.event[i].venue.uri);
-
-//    //EVENT LOCATION -- new <td>
-//    console.log(data..resultsPage.results.event[i].location.city);
-// }
